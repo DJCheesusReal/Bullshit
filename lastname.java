a7ff8a1072e94d9e098ef656ae8301dc19e12adb
Will Yates <134006619+DJCheesusReal@users.noreply.github.com>
2026-09-24 10:50:05 +0000

    hello

--- lastname.java
+++ lastname.java
@@ -2,21 +2,27 @@ import java.util.Scanner;
 
 public class lastname {
 
-    public static void main(String[] args) throws Exception {
-        int split = 0;
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("input your full name");
-        String name = input.nextLine();
-        for (int i = 0; i < name.length(); i++){
-            if (name.charAt(i).equals(" ")) && i != 0){
+        String name = input.nextLine().trim();
+
+        int split = -1;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == ' ' && i != 0) {
                 split = i;
             }
-
         }
-        String first = name.substring(0,split);
-        String last = name.substring(split);
-        System.out.println("Hello " + first + last);
 
+        if (split == -1) {
+            System.out.println("Hello " + name);
+            return;
+        }
+        
+        String first = name.substring(0, split);
+        String last = name.substring(split + 1);
+        System.out.println("Hello " + first + " " + last);
+        System.out.println("Your last name is " + last);
+        input.close();
     }
-
 }
